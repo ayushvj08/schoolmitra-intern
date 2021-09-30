f7b4b5f5b1b5781432715d37ddb63ca466925a12
Ayush Vijayant <ayush.vijayant@gmail.com>
2021-09-30 12:59:50 +0530

    remove commented routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,4 @@ Rails.application.routes.draw do
   get "/certificate", to: "home#show_certificate"
   devise_for :users
   resource :student, controller: "student"
-
-  # get "/student_create", to: "student#create_form"
-  # post "/student_create", to: "student#create"
-
-  # get "/student_update", to: "student#update_form"
-  # put "/student_update", to: "student#update"
 end
